84f7369d6df28c82abb6e1f6d314697402de7ddb
Sahil Pathak <159657642+Coder-1969@users.noreply.github.com>
2025-01-07 18:00:12 +0000

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,36 +1,37 @@
+package Model;
+
 public class Customer {
     private String firstName;
     private String lastName;
     private String parcelID;
 
+    // Constructor
     public Customer(String firstName, String lastName, String parcelID) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.parcelID = parcelID;
     }
 
+    // Getter for full name
     public String getFullName() {
-        return firstName + " " + lastName; // Combine with a space
+        return firstName + " " + lastName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Getter for parcel ID
     public String getParcelID() {
         return parcelID;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setParcelID(String parcelID) {
-        this.parcelID = parcelID;
-    }
-
     @Override
     public String toString() {
-        return "Customer [FullName= " + getFullName() + ", ParcelID= " + parcelID + "]";
+        return firstName + " " + lastName + " (" + parcelID + ")";
     }
 }
